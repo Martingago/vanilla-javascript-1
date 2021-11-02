289a110ba6ede3f288c322550e0cfd1af8c6d2b5
Martin <UsuarioLocal@EQUIP003028.xunta.local>
2021-11-02 09:40:24 +0100

    creado el js del ejercicio
    aaaaaaaaaaaaaaaaaaaaaa

---
+++ js/app.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// Ejercicio 1
+
+var  miNombre = ("Martín Gago");
+console.log(miNombre)
+
+// Ejercicio 2
+
+miNombre = ("Chorén");
+console.log(miNombre)
+
+// Ejercicio 3
+
+/* Si comentamos la variable creada anteriormente, la consola nos va a mostrar un error, 
+ya que nuestra variable no ha sido definida "Uncaught ReferenceError: miNombre is not defined"
+*/
+
+// Ejercicio 4
+
+let operador1 = 5;
+let operador2 = 6;
+let resultado = 0;
+
+resultado = operador1+operador2;
+
+console.log(resultado)
